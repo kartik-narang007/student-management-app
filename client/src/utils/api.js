14f859e8c90a78b8521b04ee78e3065546838fa8
Kartik Narang <narangk007@gmail.com>
2024-09-01 13:09:35 +0530

    Configured for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:5000";
+export const HOST = process.env.HOST;
 export const API = `${HOST}/api`;
 
 export const FETCH_CLASS_NAMES = `${API}/fetch-class-names`;
