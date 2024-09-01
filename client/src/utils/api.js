d4c3ed7359d2e8c2d8d358720afcb05d69aba3e4
Kartik Narang <narangk007@gmail.com>
2024-09-01 13:53:37 +0530

    Updations

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,7 @@
-export const URL = process.env.URL;
+export const URL = process.env.REACT_APP_URL;
+
+
 export const API = `${URL}/api`;
+
 
 export const FETCH_CLASS_NAMES = `${API}/fetch-class-names`;
